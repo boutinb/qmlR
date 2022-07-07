90c4ba81b9172a643cdafbff5bc389bb17c0f11e
boutinb <b.boutin@uva.nl>
2022-07-07 17:56:31 +0200

    Add QML stuff

--- src/rcpp_hello_world.cpp
+++ src/rcpp_hello_world.cpp
@@ -9,33 +9,46 @@ using namespace Rcpp;
 #include <QQmlComponent>
 #include <QGuiApplication>
 #include <QQuickRenderControl>
+#include <QQuickItem>
+#include "appdirs.h"
+#include "checkboxbase.h"
+
 
 // [[Rcpp::export]]
 StringVector runQml(String qmlFilePath) 
 {
 	int					dummyArgc = 1;
-	char				dummyArgv[2];
-	dummyArgv[0] = '?';
-	dummyArgv[1] = '\0';
+	char			**	dummyArgv = new char*[1];
+	dummyArgv[0] = new char[2];
+	//dummyArgv[1] = new char[8];
 
-	char			*	dummyArgvP = dummyArgv;
-	QGuiApplication		application(dummyArgc, &dummyArgvP);
+	memcpy(dummyArgv[0], "?", 2);
+	//memcpy(dummyArgv[1], "minimal", 8);
+
+	QGuiApplication		application(dummyArgc, dummyArgv);
+
 	QQuickRenderControl renderControl;
 	QQuickWindow		quickWindow(&renderControl);
 	QQmlEngine 			qmlEngine;
+	qmlRegisterType<CheckBoxBase>								("JASP",		1, 0, "CheckBoxBase"					);
+
 	QFileInfo			qmlFile(QString::fromStdString(qmlFilePath));
 	QString				qmlBaseName(qmlFile.baseName());	
 
 	QQmlComponent		qmlComp(&qmlEngine, QUrl::fromLocalFile(qmlFile.absoluteFilePath()), QQmlComponent::PreferSynchronous);
 
-	bool worked = qmlComp.create();
+	QQuickItem* obj = qobject_cast<QQuickItem*>(qmlComp.create());
 
 	StringVector output;
+	output.push_back(AppDirs::documents().toStdString());
 
 	for(const auto & error : qmlComp.errors())
 		output.push_back("Error in '" + qmlBaseName.toStdString() + "' at " + std::to_string(error.line()) + "," + std::to_string(error.column()) + ": " + error.description().toStdString());
 		
-	output.push_back(worked ? "Could load QML satisfactorily" : "Loading QML failed miserably");
+	output.push_back(obj ? "Could load QML satisfactorily" : "Loading QML failed miserably");
+
+	if (obj)
+		output.push_back(obj->property("myProperty").toString().toStdString());
     
 	return output;
 }
--- tests/testthat/TestMeCorrect.qml
+++ tests/testthat/TestMeCorrect.qml
@@ -1,7 +1,16 @@
 import QtQuick 2.15
+import JASP.Controls 1.0
 
 Item
 {
 	id: someID
+
+	property string myProperty: "Hello QML World"
+
+	CheckBox
+	{
+		name: "test"
+		isBound: false
+	}
 
 }
